851378d4bee7d3854d97210516558733e5e16d76
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-08-03 00:04:09 +0300

    refactoring interface

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,69 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartOne extends CoursePartBase {
+  name: "Fundamentals";
+  description: string;
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase {
+  name: "Deeper type usage";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
   ];
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  courseParts.forEach(part => {
+    switch (part.name) {
+      case "Fundamentals":
+        console.log(part.description);
+        break;
+      case "Using props to pass data":
+        break;
+      case "Deeper type usage":
+        break;
+      default:
+        return assertNever(part);
+    }
+  });
 
   return (
     <div>
